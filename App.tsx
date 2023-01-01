2c84dcdf5f8e2185f03203a3bffbfe791daf0e65
Juan Diego Martinez <juan.martinez@btilab.co>
2023-01-01 14:28:35 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import auth from '@react-native-firebase/auth';
+
+export default function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
+}
